22244a6b91d5d0dc83cc21ea0b8dd964e94eb673
BOSCO0109 <boscodon321@gmail.com>
2025-07-17 01:35:33 +0530

    Update README.py
    password change

--- README.py
+++ README.py
@@ -13,6 +13,6 @@ A = ps.read_csv(raw_data)                      #This line of code is used to rea
 data = ps.DataFrame(A)                         #This line of code is used to change the raw file data into dataframe using pandas
 print(data.info())                             #This line of code is used to check the data haave any null values using default pack
 print(data.describe())                         #This line of code is used to check the min,std and max values the data have     
-engine = create_engine('mysql+mysqlconnector://root:1234567890@localhost:3306/bot') #This is called as engine used to connect with sql database through python query
+engine = create_engine('mysql+mysqlconnector://root:password@localhost:3306/bot') #This is called as engine used to connect with sql database through python query
 sqlconnector = data.to_sql('Traffic_police_project',con=engine,index=False,if_exists='replace') #This line is used to push the data to mysql connector
 print('sucessfuly upddated to sql')            #This line is used to print the statement after the date has been uploaded
